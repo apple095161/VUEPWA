4991ec2c85e3a3b95c920fdbf2e253046c25821c
denwei136@outlook.com <denwei136@outlook.com>
2022-05-10 15:18:49 +0800

    errewjrew

---
+++ precache-manifest.baee327229a526569f340cb5318117b2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c430b3b69ed2d53ae2ee",
+    "url": "/css/app.735dcad9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "65a702dc382c3a9cd3c77c96df9dc6fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba3bdd2882841b4d18f8",
+    "url": "/js/about.1c63f253.js"
+  },
+  {
+    "revision": "c430b3b69ed2d53ae2ee",
+    "url": "/js/app.16ea9ed8.js"
+  },
+  {
+    "revision": "f1e9df41e4b9b5876f45",
+    "url": "/js/chunk-vendors.0c0e4656.js"
+  },
+  {
+    "revision": "735beb9b1c9deee1e02255eaad6465fd",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
